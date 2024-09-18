49e318ff6da183fee81f4ac8ca3bec47671fb1cb
老房东 <huangdong@gmail.com>
2024-09-18 15:08:35 -0400

    Email notifaction support on register

---
+++ src/main/java/org/hdcola/carnet/Service/EmailService.java
@@ -0,0 +1,89 @@
+package org.hdcola.carnet.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.hdcola.carnet.Entity.Role;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+public class EmailService {
+
+    @Value("${carnet.admin.email}")
+    private String adminEmail;
+
+    @Value("${carnet.admin.notification.emailfrom}")
+    private String emailFrom;
+
+    private final JavaMailSender mailSender;
+
+    private final TemplateEngine templateEngine;
+
+    public EmailService(JavaMailSender mailSender, TemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
+
+    public void sendSimpleEmailToAdmin(String subject, String body) {
+        sendSimpleEmail(adminEmail, subject, body);
+    }
+    public void sendSimpleEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        message.setFrom(emailFrom);
+
+        mailSender.send(message);
+        log.debug("Email sent to: {} : {}\n{}", toEmail, subject, body);
+    }
+
+    public void sendThymeleafMailToAdmin(String subject, String template, Map<String, Object> templateModel) throws MessagingException {
+        sendThymeleafMail(adminEmail, subject, template, templateModel);
+    }
+    public void sendThymeleafMail(String toEmail, String subject, String template, Map<String, Object> templateModel) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        helper.setFrom(emailFrom);
+
+        Context context = new Context();
+        context.setVariables(templateModel);
+        String html = templateEngine.process(template, context);
+        helper.setText(html, true);
+
+        mailSender.send(message);
+        log.debug("Email sent to: {} : {}\n{}", toEmail, subject, html);
+    }
+
+
+    public void sendWelcomeEmail(String toEmail, String name, Role role){
+        sendSimpleEmailToAdmin("New user registered: " + toEmail, role.name() + " have a new user registered");
+        sendWelcomeEmail(toEmail, name);
+    }
+
+    private void sendWelcomeEmail(String toEmail, String name) {
+        String subject = "Welcome to Carnet";
+        String template = "email/welcome";
+        Map<String, Object> templateModel = Map.of(
+                "title", name,
+                "message", "Welcome to Carnet"
+        );
+        try {
+            sendThymeleafMail(toEmail, subject, template, templateModel);
+        } catch (MessagingException e) {
+            log.error("Error sending email", e);
+        }
+    }
+}
