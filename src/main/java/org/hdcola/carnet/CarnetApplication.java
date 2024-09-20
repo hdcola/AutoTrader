730b05bcda0489caa126452be087473e383d57b6
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2024-09-20 12:12:16 -0400

    Update CarnetApplication.java

--- src/main/java/org/hdcola/carnet/CarnetApplication.java
+++ src/main/java/org/hdcola/carnet/CarnetApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication(exclude = {
-        com.google.cloud.spring.autoconfigure.core.GcpContextAutoConfiguration.class
-})
+@SpringBootApplication()
 public class CarnetApplication {
 
     public static void main(String[] args) {
