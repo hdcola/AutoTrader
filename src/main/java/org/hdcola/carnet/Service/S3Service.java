697bbb0316f4896ebcdcc754753bb9bd32a56871
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2024-09-21 10:03:20 -0400

    Update S3Service.java

--- src/main/java/org/hdcola/carnet/Service/S3Service.java
+++ src/main/java/org/hdcola/carnet/Service/S3Service.java
@@ -3,11 +3,13 @@ package org.hdcola.carnet.Service;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 
+@Slf4j
 @Service
 public class S3Service {
 
