4f84683d861e3bc7b05c3284a40b14f461f140ec
kirillKnyaz <161050906+kirillKnyaz@users.noreply.github.com>
2024-09-22 20:16:23 -0400

    api call successful

--- src/main/java/org/hdcola/carnet/Service/VinDecodeService.java
+++ src/main/java/org/hdcola/carnet/Service/VinDecodeService.java
@@ -1,12 +1,25 @@
 package org.hdcola.carnet.Service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class VinDecodeService {
 
-    public String decodeVin(String vin) {
+    private final String API_URL = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/{vin}?format=json";
+    private final WebClient webClient;
 
-        return vin + " decoded";
+    public VinDecodeService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://vpic.nhtsa.dot.gov").build();
+    }
+
+    public String decodeVin(String vin) {
+        String response = webClient.get()
+                .uri(API_URL, vin)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
     }
 }
